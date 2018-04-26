a592db07727aedfd6b16e0934cd9e2ae1f7ea7b5
Brent Perry <burentoperii@gmail.com>
2018-04-26 10:01:11 -0500

    initialize repo

---
+++ PingPong.cs
@@ -0,0 +1,35 @@
+using System;
+
+class PingPong
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        string stringInputNumber = Console.ReadLine();
+        int inputNumber = int.Parse(stringInputNumber);
+        pingPong(inputNumber);
+    }
+
+    static void pingPong(int number)
+    {
+        for (int i = 0; i <= number; i++)
+        {
+            if (i % 15 == 0)
+            {
+                Console.WriteLine("ping-pong");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("pong");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("ping");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
